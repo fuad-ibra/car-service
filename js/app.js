3801b56dcac087a82b8704566e810b57289feb5b
Fuad Ibrahimov <86367621+fuad-ibra@users.noreply.github.com>
2022-09-02 18:21:15 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -759,8 +759,8 @@
                         behavior: "smooth"
                     });
                 }
-                functions_FLS(`[gotoBlock]: Юхуу...едем к ${targetBlock}`);
-            } else functions_FLS(`[gotoBlock]: Ой ой..Такого блока нет на странице: ${targetBlock}`);
+                functions_FLS(`[gotoBlock]: ${targetBlock}`);
+            } else functions_FLS(`[gotoBlock]: Такого блока нет на странице: ${targetBlock}`);
         };
         function formRating() {
             const ratings = document.querySelectorAll(".rating");
@@ -4523,100 +4523,6 @@
         window.addEventListener("load", (function(e) {
             initSliders();
         }));
-        class ScrollWatcher {
-            constructor(props) {
-                let defaultConfig = {
-                    logging: true
-                };
-                this.config = Object.assign(defaultConfig, props);
-                this.observer;
-                !document.documentElement.classList.contains("watcher") ? this.scrollWatcherRun() : null;
-            }
-            scrollWatcherUpdate() {
-                this.scrollWatcherRun();
-            }
-            scrollWatcherRun() {
-                document.documentElement.classList.add("watcher");
-                this.scrollWatcherConstructor(document.querySelectorAll("[data-watch]"));
-            }
-            scrollWatcherConstructor(items) {
-                if (items.length) {
-                    this.scrollWatcherLogging(`Проснулся, слежу за объектами (${items.length})...`);
-                    let uniqParams = uniqArray(Array.from(items).map((function(item) {
-                        return `${item.dataset.watchRoot ? item.dataset.watchRoot : null}|${item.dataset.watchMargin ? item.dataset.watchMargin : "0px"}|${item.dataset.watchThreshold ? item.dataset.watchThreshold : 0}`;
-                    })));
-                    uniqParams.forEach((uniqParam => {
-                        let uniqParamArray = uniqParam.split("|");
-                        let paramsWatch = {
-                            root: uniqParamArray[0],
-                            margin: uniqParamArray[1],
-                            threshold: uniqParamArray[2]
-                        };
-                        let groupItems = Array.from(items).filter((function(item) {
-                            let watchRoot = item.dataset.watchRoot ? item.dataset.watchRoot : null;
-                            let watchMargin = item.dataset.watchMargin ? item.dataset.watchMargin : "0px";
-                            let watchThreshold = item.dataset.watchThreshold ? item.dataset.watchThreshold : 0;
-                            if (String(watchRoot) === paramsWatch.root && String(watchMargin) === paramsWatch.margin && String(watchThreshold) === paramsWatch.threshold) return item;
-                        }));
-                        let configWatcher = this.getScrollWatcherConfig(paramsWatch);
-                        this.scrollWatcherInit(groupItems, configWatcher);
-                    }));
-                } else this.scrollWatcherLogging("Сплю, нет объектов для слежения. ZzzZZzz");
-            }
-            getScrollWatcherConfig(paramsWatch) {
-                let configWatcher = {};
-                if (document.querySelector(paramsWatch.root)) configWatcher.root = document.querySelector(paramsWatch.root); else if ("null" !== paramsWatch.root) this.scrollWatcherLogging(`Эмм... родительского объекта ${paramsWatch.root} нет на странице`);
-                configWatcher.rootMargin = paramsWatch.margin;
-                if (paramsWatch.margin.indexOf("px") < 0 && paramsWatch.margin.indexOf("%") < 0) {
-                    this.scrollWatcherLogging(`Ой ой, настройку data-watch-margin нужно задавать в PX или %`);
-                    return;
-                }
-                if ("prx" === paramsWatch.threshold) {
-                    paramsWatch.threshold = [];
-                    for (let i = 0; i <= 1; i += .005) paramsWatch.threshold.push(i);
-                } else paramsWatch.threshold = paramsWatch.threshold.split(",");
-                configWatcher.threshold = paramsWatch.threshold;
-                return configWatcher;
-            }
-            scrollWatcherCreate(configWatcher) {
-                this.observer = new IntersectionObserver(((entries, observer) => {
-                    entries.forEach((entry => {
-                        this.scrollWatcherCallback(entry, observer);
-                    }));
-                }), configWatcher);
-            }
-            scrollWatcherInit(items, configWatcher) {
-                this.scrollWatcherCreate(configWatcher);
-                items.forEach((item => this.observer.observe(item)));
-            }
-            scrollWatcherIntersecting(entry, targetElement) {
-                if (entry.isIntersecting) {
-                    !targetElement.classList.contains("_watcher-view") ? targetElement.classList.add("_watcher-view") : null;
-                    this.scrollWatcherLogging(`Я вижу ${targetElement.classList}, добавил класс _watcher-view`);
-                } else {
-                    targetElement.classList.contains("_watcher-view") ? targetElement.classList.remove("_watcher-view") : null;
-                    this.scrollWatcherLogging(`Я не вижу ${targetElement.classList}, убрал класс _watcher-view`);
-                }
-            }
-            scrollWatcherOff(targetElement, observer) {
-                observer.unobserve(targetElement);
-                this.scrollWatcherLogging(`Я перестал следить за ${targetElement.classList}`);
-            }
-            scrollWatcherLogging(message) {
-                this.config.logging ? functions_FLS(`[Наблюдатель]: ${message}`) : null;
-            }
-            scrollWatcherCallback(entry, observer) {
-                const targetElement = entry.target;
-                this.scrollWatcherIntersecting(entry, targetElement);
-                targetElement.hasAttribute("data-watch-once") && entry.isIntersecting ? this.scrollWatcherOff(targetElement, observer) : null;
-                document.dispatchEvent(new CustomEvent("watcherCallback", {
-                    detail: {
-                        entry
-                    }
-                }));
-            }
-        }
-        modules_flsModules.watcher = new ScrollWatcher({});
         let addWindowScrollEvent = false;
         function pageNavigation() {
             document.addEventListener("click", pageNavigationAction);
@@ -4628,7 +4534,7 @@
                         const gotoLink = targetElement.closest("[data-goto]");
                         const gotoLinkSelector = gotoLink.dataset.goto ? gotoLink.dataset.goto : "";
                         const noHeader = gotoLink.hasAttribute("data-goto-header") ? true : false;
-                        const gotoSpeed = gotoLink.dataset.gotoSpeed ? gotoLink.dataset.gotoSpeed : 500;
+                        const gotoSpeed = gotoLink.dataset.gotoSpeed ? gotoLink.dataset.gotoSpeed : 1e3;
                         const offsetTop = gotoLink.dataset.gotoTop ? parseInt(gotoLink.dataset.gotoTop) : 0;
                         gotoblock_gotoBlock(gotoLinkSelector, noHeader, gotoSpeed, offsetTop);
                         e.preventDefault();
@@ -4660,7 +4566,7 @@
             addWindowScrollEvent = true;
             const header = document.querySelector("header.header");
             const headerShow = header.hasAttribute("data-scroll-show");
-            const headerShowTimer = header.dataset.scrollShow ? header.dataset.scrollShow : 500;
+            const headerShowTimer = header.dataset.scrollShow ? header.dataset.scrollShow : 300;
             const startPoint = header.dataset.scroll ? header.dataset.scroll : 1;
             let scrollDirection = 0;
             let timer;
